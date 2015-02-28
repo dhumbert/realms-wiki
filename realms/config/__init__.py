9255542298f09e587df4f33baf6c540a436a0363
Devin Humbert <devin.humbert@gmail.com>
2015-02-27 20:21:05 -0800

    Change order of config load to something sane.

--- realms/config/__init__.py
+++ realms/config/__init__.py
@@ -39,7 +39,7 @@ def save(conf):
 def get_path(check_write=False):
     """Find config path
     """
-    for loc in "/etc/realms-wiki", os.path.expanduser("~"), os.curdir:
+    for loc in os.curdir, "/etc/realms-wiki", os.path.expanduser("~"):
         if not loc:
             continue
         path = os.path.join(loc, "realms-wiki.json")
@@ -119,6 +119,9 @@ WIKI_PATH = '/tmp/wiki'
 
 # Name of page that will act as home
 WIKI_HOME = 'home'
+
+# Whether to show live preview automatically
+AUTOMATIC_LIVE_PREVIEW = False
 
 ALLOW_ANON = True
 REGISTRATION_ENABLED = True
